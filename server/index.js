1a15a1ccd221ee21082ca362f013e8212a2e189c
YelloFattyBean <jesse.geiss@gmail.com>
2020-09-25 16:40:10 -0700

    change webpack bundle location

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const Model = require('../database/model.js');
 
 const app = express();
 const PORT = 3002;
-const PUBLIC_DIR = '../public';
-
-app.use(express.static(PUBLIC_DIR));
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
