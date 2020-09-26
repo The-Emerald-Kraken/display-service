f62d8693aa6bef129fe823410736cc2db8a15337
YelloFattyBean <jesse.geiss@gmail.com>
2020-09-25 19:10:48 -0700

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,15 @@ const DIST_DIR = path.join(__dirname, '/public');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
     path: DIST_DIR,
+    filename: 'bundle.js',
   },
   module: {
     rules: [
-      { test: / .jsx?$/, use: 'babel-loader' },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+      },
     ],
   },
   mode: 'development',
